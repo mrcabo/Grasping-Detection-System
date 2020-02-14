8e88462f8389df814b385a47cc10375f0b8c481c
anpenta <anpenta01@gmail.com>
2020-02-14 20:25:52 +0100

    incorporate cnn into existing code

--- code/cnn.py
+++ code/cnn.py
@@ -11,14 +11,16 @@ class Net(nn.Module):
 
   def __init__(self):
     super(Net, self).__init__()
-    self.conv1 = nn.Conv2d(3, 32, 3)
-    self.conv2 = nn.Conv2d(32, 64, 3)
-    self.fc1 = nn.Linear(232960, 256)
-    self.fc2 = nn.Linear(256, 5)
+    self.conv1 = nn.Conv2d(3, 16, 5)
+    self.conv2 = nn.Conv2d(16, 32, 5)
+    self.conv3 = nn.Conv2d(32, 64, 5)
+    self.fc1 = nn.Linear(512, 1024)
+    self.fc2 = nn.Linear(1024, 5)
 
   def forward(self, x):
-    x = nn.functional.max_pool2d(nn.functional.relu(self.conv1(x)), (3, 3))
-    x = nn.functional.max_pool2d(nn.functional.relu(self.conv2(x)), (3, 3))
+    x = nn.functional.max_pool2d(nn.functional.relu(self.conv1(x)), (5, 5))
+    x = nn.functional.max_pool2d(nn.functional.relu(self.conv2(x)), (5, 5))
+    x = nn.functional.max_pool2d(nn.functional.relu(self.conv3(x)), (5, 5))
     x = x.view(x.shape[0], -1)
     x = nn.functional.relu(self.fc1(x))
     x = torch.sigmoid(self.fc2(x))
